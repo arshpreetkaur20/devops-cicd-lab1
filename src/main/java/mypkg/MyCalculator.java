ff81acec039505366ba2293c68e3ac3b4ef835fd
Arshpreetexp1 <500094140@stu.upes.ac.in>
2023-08-25 09:52:12 +0530

    first version

---
+++ src/main/java/mypkg/MyCalculator.java
@@ -0,0 +1,27 @@
+package mypkg;
+
+public class MyCalculator {
+	
+	public int sum(int a, int b) {
+		return(a+b);
+	}
+	public int diff(int a, int b) {
+		return(a-b);
+	}
+	public int mul(int a, int b) {
+		return(a*b);
+	}
+	public int div(int a, int b) {
+		return(a/b);
+	}
+	public int rem(int a, int b) {
+		return(a%b);
+	}
+	public static void main(String[] args) {
+		MyCalculator calc = new MyCalculator();
+		System.out.println("Sum is: "+calc.sum(20,10));
+		System.out.println("diff is: "+calc.diff(20,10));
+
+	}
+
+}
