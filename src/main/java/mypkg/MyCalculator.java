2dfc434c020e9e7eb29abebae5b6d3811b064e93
Arshpreet <arshpreetk498@gmail.com>
2023-08-26 14:33:23 +0530

    third version

--- src/main/java/mypkg/MyCalculator.java
+++ src/main/java/mypkg/MyCalculator.java
@@ -8,11 +8,15 @@ public class MyCalculator {
 	public int diff(int a, int b) {
 		return(a-b);
 	}
-	
+	public int mul(int a, int b) {
+		return(a*b);
+	}
 	public static void main(String[] args) {
 		MyCalculator calc = new MyCalculator();
 		System.out.println("Sum is: "+calc.sum(50,40));
 		System.out.println("diff is: "+calc.diff(20,10));
+		System.out.println("diff is: "+calc.mul(20,10));
+
 
 	}
 
